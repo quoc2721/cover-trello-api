7499a6e55726eed4a1f636f9f9b013c14c612a26
Quốc Lê <quoc020721@gmail.com>
2022-07-23 13:14:56 +0700

    Init Project

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello World</h1>')
+})
+
+app.listen(port, hostname), () => [
+    console.log(`Hello run ${hostname}:${port}/`)
+]
