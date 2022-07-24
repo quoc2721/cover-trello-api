521b551d83f4080d9ebd75ec462b917ec7255e30
Quốc Lê <quoc020721@gmail.com>
2022-07-24 15:24:49 +0700

    Connect MOngooDB success

---
+++ src/config/environtment.js
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+
+export const env = {
+    MONGODB_URI : process.env.MONGODB_URI,
+    HOST: process.env.HOST,
+    POST: process.env.POST
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from 'express'
+import { connectDB } from './config/mongoDB.js'
+import { env } from './config/environtment.js'
+
 
 const app = express()
 
-const hostname = 'localhost'
-const port = 5000
+
+connectDB().catch(console.log)
 
 app.get('/', (req, res) => {
     res.end('<h1>Hello World Van Quoc</h1>')
 })
 
-app.listen(port, hostname), () => [
-    console.log(`Hello run ${hostname}:${port}/`)
+app.listen(env.POST, env.HOST), () => [
+    console.log(`Hello run ${env.HOST}:${env.POST}/`)
 ]
